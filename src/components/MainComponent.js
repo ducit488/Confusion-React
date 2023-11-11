b3e74c6c66cecd8270888ce2f3a283ce5a632ed7
ducit488 <ducit488@gmail.com>
2023-11-11 07:46:01 +0700

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,6 +13,7 @@ import HomeComponent from "./HomeComponent";
 import DishdetailComponent from "./DishdetailComponent";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 import {
   fetchDishes,
@@ -21,6 +22,7 @@ import {
   addComment,
 } from "../store";
 import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function MainComponent() {
   const { leaders } = useSelector((state) => {
@@ -54,76 +56,83 @@ function MainComponent() {
   const resetFeedbackForm = () => {
     dispatch(actions.reset("feedback"));
   };
+  const location = useLocation();
 
   return (
     <div>
       <HeaderComponent />
       <div>
-        <Switch>
-          <Route
-            path="/home"
-            component={() => (
-              <HomeComponent
-                dish={dishesData.filter((dish) => dish.featured)[0]}
-                dishesLoading={dishesLoading}
-                dishesError={dishesError}
-                promotion={promotionsData.filter((promo) => promo.featured)[0]}
-                promotionsLoading={promotionsLoading}
-                promotionsError={promotionsError}
-                leader={leaders.filter((leader) => leader.featured)[0]}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/aboutus"
-            component={() => <AboutComponent leaders={leaders} />}
-          />
-
-          <Route
-            exact
-            path="/menu"
-            component={() => (
-              <MenuComponent
-                dishes={dishesData}
-                dishesLoading={dishesLoading}
-                dishesError={dishesError}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/menu/:dishId"
-            render={({ match }) => (
-              <DishdetailComponent
-                dish={
-                  dishesData.filter(
-                    (dish) => dish.id === parseInt(match.params.dishId, 10)
-                  )[0]
-                }
-                dishesLoading={dishesLoading}
-                dishesError={dishesError}
-                comments={commentsData?.filter(
-                  (comment) =>
-                    comment.dishId === parseInt(match.params.dishId, 10)
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="page" timeout={300}>
+            <Switch location={location}>
+              <Route
+                path="/home"
+                component={() => (
+                  <HomeComponent
+                    dish={dishesData.filter((dish) => dish.featured)[0]}
+                    dishesLoading={dishesLoading}
+                    dishesError={dishesError}
+                    promotion={
+                      promotionsData.filter((promo) => promo.featured)[0]
+                    }
+                    promotionsLoading={promotionsLoading}
+                    promotionsError={promotionsError}
+                    leader={leaders.filter((leader) => leader.featured)[0]}
+                  />
                 )}
-                commentsLoading={commentsLoading}
-                commentsError={commentsError}
-                addComment={addComment}
-                commentsLen={commentsData.length}
               />
-            )}
-          />
+              <Route
+                exact
+                path="/aboutus"
+                component={() => <AboutComponent leaders={leaders} />}
+              />
 
-          <Route
-            exact
-            path="/contactus"
-            component={() => (
-              <ContactComponent resetFeedbackForm={resetFeedbackForm} />
-            )}
-          />
-          <Redirect to="/home" />
-        </Switch>
+              <Route
+                exact
+                path="/menu"
+                component={() => (
+                  <MenuComponent
+                    dishes={dishesData}
+                    dishesLoading={dishesLoading}
+                    dishesError={dishesError}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/menu/:dishId"
+                render={({ match }) => (
+                  <DishdetailComponent
+                    dish={
+                      dishesData.filter(
+                        (dish) => dish.id === parseInt(match.params.dishId, 10)
+                      )[0]
+                    }
+                    dishesLoading={dishesLoading}
+                    dishesError={dishesError}
+                    comments={commentsData?.filter(
+                      (comment) =>
+                        comment.dishId === parseInt(match.params.dishId, 10)
+                    )}
+                    commentsLoading={commentsLoading}
+                    commentsError={commentsError}
+                    addComment={addComment}
+                    commentsLen={commentsData.length}
+                  />
+                )}
+              />
+
+              <Route
+                exact
+                path="/contactus"
+                component={() => (
+                  <ContactComponent resetFeedbackForm={resetFeedbackForm} />
+                )}
+              />
+              <Redirect to="/home" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
       <FooterComponent />
     </div>
